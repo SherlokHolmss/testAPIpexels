0e23ca80e02afcfcd8bc2c72a1e98966696d010b
Davud <david.poninka@gmail.com>
2025-03-16 13:52:54 +0200

    psuh

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const list = document.querySelector(".list");
 const button = document.querySelector(".button");
 
-function renderItems(){
-    
+const apiKey = "49368404-24cee39adef47ce95f9739319";
+
+
+function render(items){
+    const html = items.hits.map(item => `<li><img src = "${item.userImageURL}">${item.tags}</li>`).join("");
+    list.innerHTML = html;
 }
-
-
-
-fetch('https://jsonplaceholder.typicode.com/posts/')
+button.addEventListener("click", ()=>{
+    fetch(`https://pixabay.com/api/?key=${apiKey}&q=cat&image_type=photo`)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => render(json));
+});
+
 
 
